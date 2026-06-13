a89f53cff95239351b3feaa8f529168d1ec58d7c
hattoben <benjamin.hatto@gmail.com>
2026-06-13 01:23:04 -0400

    Initial commit

---
+++ HelloWorld/HelloWorld.ino
@@ -0,0 +1,16 @@
+// ECE 430 Project 6
+// Benjamin Hatto
+// SSD1306 "Hello World"
+
+#include <Wire.h>
+#include <Adafruit_SSD
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
